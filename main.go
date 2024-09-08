5b50474afeb337177ab8c5fc174a19ab69257214
BaDelhommer <badelhommer@gmail.com>
2024-09-08 11:40:17 -0500

    added command line args processing to main function

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	rawURL := "https://blog.boot.dev/path/"
-	normalizeURL(rawURL)
+	args := os.Args
+	switch {
+	case len(args) < 2:
+		fmt.Println("no website provided")
+		os.Exit(1)
+	case len(args) > 2:
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	default:
+		fmt.Println("starting crawl of: ", args[1])
+	}
 }
